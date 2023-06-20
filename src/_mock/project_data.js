3c239f40c35fea33be59963641e82cee1c0c0da5
quyld17 <quyld17@gmail.com>
2023-06-20 16:20:44 +0700

    creating data

--- src/_mock/project_data.js
+++ src/_mock/project_data.js
@@ -22,11 +22,44 @@ export const projectDetail = [
     task: [
       {
         name: 'Code refactoring',
-        detail: '123',
+        detail: 'Rename the files and folders. Improve code structure and organization.',
       },
       {
         name: 'Database design',
-        detail: '123',
+        detail: 'Design the database schema. Optimize data storage and retrieval.',
+      },
+      {
+        name: 'Payment integration',
+        detail: 'Integrate payment gateway for secure and seamless transactions. Handle different payment methods.',
+      },
+      {
+        name: 'Product listing page',
+        detail:
+          'Create a page to display products with proper filtering and sorting functionality. Enhance user experience.',
+      },
+      {
+        name: 'User authentication',
+        detail: 'Implement user registration and login system. Manage user roles and permissions.',
+      },
+      {
+        name: 'Shopping cart functionality',
+        detail: 'Build a shopping cart to add and manage products. Implement cart calculations and checkout process.',
+      },
+      {
+        name: 'Order management system',
+        detail: 'Develop a system to manage and track customer orders. Handle order processing and status updates.',
+      },
+      {
+        name: 'Inventory management',
+        detail: 'Create a system to track and manage product inventory. Handle stock updates and notifications.',
+      },
+      {
+        name: 'Search functionality',
+        detail: 'Implement a search feature for products. Enable efficient search with relevant results.',
+      },
+      {
+        name: 'Responsive design',
+        detail: 'Ensure the website works well on different devices and screen sizes. Optimize for mobile and tablets.',
       },
     ],
   },
@@ -35,11 +68,50 @@ export const projectDetail = [
     task: [
       {
         name: 'Code refactoring',
-        detail: '123',
+        detail: 'Improve the code structure for better readability and maintainability. Remove code duplication.',
       },
       {
         name: 'UI Implementing',
-        detail: '123',
+        detail:
+          'Design and implement the user interface for the messaging application. Focus on usability and aesthetics.',
+      },
+      {
+        name: 'Real-time messaging',
+        detail:
+          'Implement real-time messaging functionality using web sockets. Enable instant messaging between users.',
+      },
+      {
+        name: 'User authentication',
+        detail: 'Create a secure authentication system for users. Handle login, registration, and password recovery.',
+      },
+      {
+        name: 'Message encryption',
+        detail:
+          'Implement end-to-end encryption for secure communication. Protect user messages from unauthorized access.',
+      },
+      {
+        name: 'Notification system',
+        detail:
+          'Develop a system to send notifications for new messages and important updates. Ensure timely user notifications.',
+      },
+      {
+        name: 'File sharing',
+        detail: 'Enable users to share files with each other. Implement file upload and download functionality.',
+      },
+      {
+        name: 'Group chat functionality',
+        detail:
+          'Create the ability for users to participate in group chats. Manage group membership and message distribution.',
+      },
+      {
+        name: 'Message search',
+        detail:
+          'Implement a search feature to find specific messages or conversations. Enable quick and efficient search functionality.',
+      },
+      {
+        name: 'Message archiving',
+        detail:
+          'Develop a system to archive and store messages. Allow users to access and retrieve past conversations.',
       },
     ],
   },
@@ -48,11 +120,52 @@ export const projectDetail = [
     task: [
       {
         name: 'Code refactoring',
-        detail: '123',
+        detail: 'Improve code readability and maintainability. Optimize algorithms and data structures.',
       },
       {
         name: 'Resource collecting',
-        detail: '123',
+        detail:
+          'Collect and curate relevant data resources for training the AI chatbot. Ensure data quality and accuracy.',
+      },
+      {
+        name: 'Natural language processing',
+        detail:
+          'Implement NLP algorithms to understand and process user input. Enable intelligent conversation handling.',
+      },
+      {
+        name: 'Intent recognition',
+        detail:
+          'Develop algorithms to recognize user intents and map them to appropriate responses. Improve accuracy and response relevance.',
+      },
+      {
+        name: 'Contextual understanding',
+        detail:
+          'Build a system to understand and maintain contextual information during conversations. Enable meaningful and coherent responses.',
+      },
+      {
+        name: 'Multi-language support',
+        detail:
+          'Enable the chatbot to handle conversations in multiple languages. Implement language detection and translation capabilities.',
+      },
+      {
+        name: 'Integration with external APIs',
+        detail:
+          'Integrate with external services and APIs to provide additional functionality. Connect to weather, news, or other relevant APIs.',
+      },
+      {
+        name: 'User profiling',
+        detail:
+          'Develop a system to profile users based on their interactions. Personalize responses and recommendations.',
+      },
+      {
+        name: 'Conversation analytics',
+        detail:
+          'Implement analytics to track and analyze user conversations. Gather insights for chatbot improvement and user behavior understanding.',
+      },
+      {
+        name: 'Continuous learning',
+        detail:
+          'Enable the chatbot to learn from user interactions and improve over time. Implement feedback loops and model retraining.',
       },
     ],
   },
