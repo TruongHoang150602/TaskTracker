c4dc68a7ef3643e8cfb44e62e4cac89d938bf4c0
phuctong2002 <vanphuc15092002@gmail.com>
2023-06-25 12:51:15 +0700

    edit icon

--- src/pages/task/TaskItem.js
+++ src/pages/task/TaskItem.js
@@ -5,33 +5,57 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 
 const colors = {
-    "HIGH" : "#FF7979",
-    "MEDIUM" : "#FF7979",
-}
+  HIGH: '#FF7979',
+  MEDIUM: '#FF7979',
+};
 
-const TaskItem = ({data, show}) => {
-  
+const TaskItem = ({ data, show }) => {
   return (
-    <Card style={{height: 180}} onClick={show} className=' my-[20px] mx-[20px]' sx={{ width: 320  }}>
-      <CardActionArea style={{height: 180}}>
-        <CardContent >
-          <Typography  gutterBottom variant="h5" component="div">
+    <Card style={{ height: 180 }} onClick={show} className=" my-[20px] mx-[20px]" sx={{ width: 320 }}>
+      <CardActionArea style={{ height: 180 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
             {data.name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
             {data.project}
           </Typography>
-          <Typography className='flex justify-between items-center' variant="body2" color="text.secondary">
-            <div className='flex items-center leading-[30px] text-[#FF7979]'><AccessTimeIcon className='text-[16px] leading-[30px] text-[#FF7979]'/> <p className='m-[0px] leading-[30px]'>{data.date}</p></div>
-            <div className={`w-[80px] bg-[#FF7979] bg-[${colors[data.quality]}] h-[30px] text-center leading-[30px] rounded-[15px]`}>{data.quality}</div>
-          </Typography>
+          {data.score == null ? (
+            <Typography className="flex justify-between items-center" variant="body2" color="text.secondary">
+              <div className="flex items-center leading-[30px] text-[#FF7979]">
+                <AccessTimeIcon className="text-[16px] leading-[30px] text-[#FF7979]" />{' '}
+                <p className="m-[0px] leading-[30px]">{data.date}</p>
+              </div>
+              <div
+                className={`w-[80px] bg-[#FF7979] bg-[${
+                  colors[data.quality]
+                }] h-[30px] text-center leading-[30px] rounded-[15px]`}
+              >
+                {data.quality}
+              </div>
+            </Typography>
+          ) : (
+            <Typography className="flex justify-between items-center" variant="body2" color="text.secondary">
+              <div className="flex items-center leading-[30px] text-[#FF7979]">
+                <FileUploadOutlinedIcon className="text-[16px] leading-[30px] text-[#FF7979]" />{' '}
+                <p className="m-[0px] leading-[30px]">{data.date}</p>
+              </div>
+              <div
+                className={`w-[80px] bg-[#FF7979] bg-[${
+                  colors[data.quality]
+                }] h-[30px] text-center leading-[30px] rounded-[15px]`}
+              >
+                {data.score}
+              </div>
+            </Typography>
+          )}
         </CardContent>
       </CardActionArea>
     </Card>
   );
 };
-
 
 export default TaskItem;
